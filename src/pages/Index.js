906250017b1e2098cfea61e5d98a524b0dba7964
Abishek Ganesh <51887281+abishek-ganesh@users.noreply.github.com>
2022-01-10 01:34:16 -0800

    Update Index.js

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -14,11 +14,11 @@ const Index = () => (
           <h2 data-testid="heading"><Link to="/">Welcome to my personal website</Link></h2>
           <p>
             <br></br>
-            I am truly grateful that you are here, spending time to get to know me. <br></br><br></br>
-            My goal for this page is to be an evolving autobiographical diary, of sorts, showcasing my
-            career, projects, journey, and passions. <br></br><br></br>
+            Thank you for being here, spending time to understand me. <br></br><br></br>
+            My goal is for this page to be an evolving autobiographical diary, of sorts, showcasing my
+            career highlights, projects, journey, and passions. <br></br><br></br>
             The technical domains I am currently most interested in are Full-Stack development, 
-            Artificial Intelligence/Machine Learning, Data Science, and Blockchain.<br></br><br></br>
+            Artificial Intelligence/Machine Learning, Data Science, and Blockchain technology.<br></br><br></br>
             I have spent most of my life focused on healthcare, education, and climate-related causes (read on to learn more),
             and am always looking for new frontiers to embark on with incredible people.<br></br><br></br>
             Speaking of which, I am always interested in opportunities to partner with amazing brands and individuals (that's you!) to solve
