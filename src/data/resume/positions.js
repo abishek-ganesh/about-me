1ffa6ecd4a9d822320ccbdb5a1c889778e1586f3
abishek-ganesh <abishek.ganesh30@gmail.com>
2022-04-25 22:49:39 -0400

    resume dates

--- src/data/resume/positions.js
+++ src/data/resume/positions.js
@@ -3,7 +3,7 @@ const positions = [
     company: 'Big Health',
     position: 'Data Science Intern',
     link: 'https://bighealth.com',
-    daterange: 'Sep 2021 - Dec 2021',
+    daterange: '2021 - 2021',
     points: [
       'Built, developed, and deployed a gradient boost classification model to predict user engagement and highlight key features with over 98% precision and 75% recall on a validation set',
       'Designed clustering algorithm to classify users based on demographics + clinical assessment responses',
@@ -13,7 +13,7 @@ const positions = [
     company: 'Big Health',
     position: 'Commercial Operations Lead',
     link: 'https://bighealth.com',
-    daterange: 'Jul 2017 - Sep 2021',
+    daterange: '2017 - 2021',
     points: [
       'Expanded coverage from to over 10 million lives globally, constituting a 1500%+ increase in revenue',
       'Led the commercial team in hiring, training, market strategy, reporting to the Board, and more',
@@ -25,7 +25,7 @@ const positions = [
     company: 'AI Camp',
     position: 'Instructor/Mentor',
     link: 'https://www.ai-camp.org/',
-    daterange: 'Sep 2021 - Present',
+    daterange: '2021 - 2022',
     points: [
       'Taught Python programming, AI/ML fundamentals, and product development to kids aged 13-18',
       'Collected training sets on the ground in Uganda. Built dashboards to visualize work of surveyors using Flask, React, and D3.',
@@ -34,7 +34,7 @@ const positions = [
   {
     company: 'Onerent',
     position: 'Director of Sales Operations',
-    daterange: 'Apr 2016 - Jan 2017',
+    daterange: '2016 - 2017',
     points: [
       'Co-led team of 15 raising bookings from $1.3 million to $3 million within 7 months',
       'Led vital projects that automated key aspects of the business to enable high-velocity growth',
@@ -47,7 +47,7 @@ const positions = [
     company: 'Aruba Networks, a HPE Company',
     position: 'Co-founder, Program Manager',
     link: 'https://ubnl.space/',
-    daterange: 'Oct 2010 – Jun 2012',
+    daterange: '2014 – 2016',
     points: [
       'Optimized data processes and analytics with CRM, SAP, and Tableau, enabling massive HPE merger',,
       'Optimized data flow through maintenance of a complex of systems including CRM, BI, and database by creating and deploying macros to save hundreds of hours of work'
