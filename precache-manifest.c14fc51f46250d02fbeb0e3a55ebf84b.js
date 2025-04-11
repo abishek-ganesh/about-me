fd314fd1244d4299258daf7f3a8caae5a0b42f33
Abishek Ganesh <dangershak@gmail.com>
2025-04-11 16:03:09 -0700

    Updates

---
+++ precache-manifest.c14fc51f46250d02fbeb0e3a55ebf84b.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd0f6afe88c7ee39a625fc3bb67d9301",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a353c178e30452dda8b",
+    "url": "/static/css/main.d1344cff.chunk.css"
+  },
+  {
+    "revision": "ac1f8e35ec5297825ba7",
+    "url": "/static/js/10.875fffd2.chunk.js"
+  },
+  {
+    "revision": "fdc87b29c5a871e78c03",
+    "url": "/static/js/11.29aa99b4.chunk.js"
+  },
+  {
+    "revision": "dead1f734d0b19e5989e",
+    "url": "/static/js/12.c9c93239.chunk.js"
+  },
+  {
+    "revision": "d04dd5f0adc634d3b127",
+    "url": "/static/js/2.e272f507.chunk.js"
+  },
+  {
+    "revision": "df9baf4220a7f050047fa3570c98d2cb",
+    "url": "/static/js/2.e272f507.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85ccc8a284bf63f2c12a",
+    "url": "/static/js/3.4c890349.chunk.js"
+  },
+  {
+    "revision": "330d9096439ec3ae33f9e38f341d6c46",
+    "url": "/static/js/3.4c890349.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b2de32104cd80bbe645",
+    "url": "/static/js/4.1164ac6b.chunk.js"
+  },
+  {
+    "revision": "c66df7736dcd8defd744",
+    "url": "/static/js/5.ef2e4669.chunk.js"
+  },
+  {
+    "revision": "84a6a77b68f972404503",
+    "url": "/static/js/6.bcc77039.chunk.js"
+  },
+  {
+    "revision": "56550efc2ceab3f578b8",
+    "url": "/static/js/7.d53c407a.chunk.js"
+  },
+  {
+    "revision": "6e2e0b2253346e3ad82a",
+    "url": "/static/js/8.5bb4db94.chunk.js"
+  },
+  {
+    "revision": "f3249b174cc832b073ab",
+    "url": "/static/js/9.d8fb94b6.chunk.js"
+  },
+  {
+    "revision": "5a353c178e30452dda8b",
+    "url": "/static/js/main.baf6e770.chunk.js"
+  },
+  {
+    "revision": "855f758a92c397f7a5e1",
+    "url": "/static/js/runtime-main.9d6909d5.js"
+  }
+]);
